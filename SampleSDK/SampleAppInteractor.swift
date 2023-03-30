3bef1570890f3819c43b4b6ac952636535864924
Akansha Srivastava <akansha.srivastava@vida.id>
2023-03-30 23:49:56 +0530

    Interactor class added

---
+++ SampleSDK/SampleAppInteractor.swift
@@ -0,0 +1,14 @@
+//
+//  SampleAppInteractor.swift
+//  SampleSDK
+//
+//  Created by Akansha Srivastava on 30/03/23.
+//
+
+import Foundation
+
+public class SampleAppInteractor {
+    public static func printData() {
+        print("Hello guys")
+    }
+}
